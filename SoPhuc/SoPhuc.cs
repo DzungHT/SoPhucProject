462d03eb5a7e5c80a0e01735e2d5825ed49d1106
DungHT <dzunght95@gmail.com>
2018-04-23 19:02:05 +0700

    commit code class SoPhuc.cs

---
+++ SoPhuc/SoPhuc.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoPhuc
+{
+    public class SoPhuc
+    {
+        /// <summary>
+        /// Phần thực
+        /// </summary>
+        public double A { get; set; }
+        /// <summary>
+        /// Phần ảo
+        /// </summary>
+        public double B { get; set; }
+
+        /// <summary>
+        /// Viết lại hàm mặc định để chuyển Số phức về dạng chuỗi
+        /// </summary>
+        /// <returns>a + bi</returns>
+        public override string ToString()
+        {
+            return string.Format("{0} + {1}i", A, B);
+        }
+
+        /// <summary>
+        /// Hàm khởi tạo
+        /// </summary>
+        public SoPhuc()
+        {
+            A = 0;
+            B = 0;
+        }
+
+        /// <summary>
+        /// Hàm khởi tạo
+        /// </summary>
+        /// <param name="a">Giá trị phần thực</param>
+        /// <param name="b">Giá trị phần ảo</param>
+        public SoPhuc(double a, double b)
+        {
+            A = a;
+            B = b;
+        }
+
+        /// <summary>
+        /// Cộng 2 số phức
+        /// </summary>
+        /// <param name="soPhuc">Số phức cần cộng</param>
+        /// <returns>Kết quả tổng của số phức hiện tại và số phức truyền vào.</returns>
+        public SoPhuc Cong(SoPhuc soPhuc)
+        {
+            SoPhuc ketQua = new SoPhuc();
+
+            ketQua.A = this.A + soPhuc.A;
+            ketQua.B = this.B + soPhuc.B;
+
+            return ketQua;
+        }
+
+
+
+    }
+}
